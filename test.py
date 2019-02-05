b776ef5d02c3852a563cc2acc94ad3be87147a40
demise712 <endaldemi@gmail.com>
2019-02-05 16:36:23 -0500

    revise file

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 #!/bin/python
 
-print a
+import unittest
+
+def PomFile(update=False):
+    from xml.etree import ElementTree as et
+    ns = "http//maven.apache.org/POM/4.0.0"
+    et.register_namespace('',ns)
+    tree = et.ElementTree()
+    tree.parse('pom.xml')
+    pom = tree.getroot().find("{%s}version"% ns)
+    if update:
+	pom.text = update
+	tree.write('pom.xml')
+    else:
+	return pom.xml
+
+print (PomFile())
+
+if __name__ == '__main__':
+    unittest.main()
